c43c6935a1fb7b3174f1ddd0481addeecf059349
Adam Davis <adavis@thesdg.com>
2018-09-26 09:36:15 -0400

    Added RxJava tests using TestSubscriber, TestObserver, and TestScheduler.

--- src/test/java/com/github/adamldavis/RxJavaDemoTest.java
+++ src/test/java/com/github/adamldavis/RxJavaDemoTest.java
@@ -1,12 +1,20 @@
 package com.github.adamldavis;
 
-import static com.github.adamldavis.DemoData.*;
-import static org.junit.Assert.*;
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.observers.TestObserver;
+import io.reactivex.schedulers.TestScheduler;
+import io.reactivex.subscribers.TestSubscriber;
+import org.junit.Test;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import org.junit.Test;
+import static com.github.adamldavis.DemoData.squares;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class RxJavaDemoTest {
 
@@ -23,6 +31,53 @@ public class RxJavaDemoTest {
                 .stream().sorted().collect(Collectors.toList());
                 
         assertArrayEquals(squares.toArray(), result.toArray());
+    }
+
+    @Test
+    public void testSubscriber() {
+        TestSubscriber<Integer> ts =
+                Flowable.range(1, 5).test();
+
+        assertEquals(5, ts.valueCount());
+    }
+
+    @Test
+    public void testSubscriberWithException() {
+        Flowable<Integer> flowable = Flowable.create(source -> {
+            source.onNext(1);
+            source.onError(new RuntimeException());
+        }, BackpressureStrategy.LATEST);
+
+        TestSubscriber<Integer> ts = flowable.test();
+
+        ts.assertSubscribed();
+        ts.assertError(RuntimeException.class);
+    }
+
+    @Test
+    public void testObserver() {
+        TestObserver<Integer> ts =
+                Observable.range(1, 5).test();
+
+        assertEquals(5, ts.valueCount());
+    }
+
+    @Test
+    public void testScheduler() {
+        TestScheduler scheduler = new TestScheduler(); //1
+        Observable<Long> tick = Observable
+                .interval(1, TimeUnit.SECONDS, scheduler); //2
+        Observable<String> observable =
+                Observable.just("foo", "bar", "biz", "baz") //3
+                .zipWith(tick, (string, index) -> index + "-" + string);//4
+        TestObserver<String> testObserver = observable
+                .subscribeOn(scheduler).test();//5
+
+        scheduler.advanceTimeBy(2300, TimeUnit.MILLISECONDS);//6
+
+        testObserver.assertNoErrors(); //7
+        testObserver.assertValues("0-foo", "1-bar");
+        testObserver.assertNotComplete();
     }
 
 }
